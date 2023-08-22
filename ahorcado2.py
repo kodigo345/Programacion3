415049d94f23f93bc6931e1d12c3bd0f7c19aeba
Kodigo345 <jesusvargasvida@gmail.com>
2023-08-22 18:29:12 -0400

    version 2023-08-22.03

--- ahorcado2.py
+++
@@ -1,6 +0,0 @@
-def jugar_ahorcado():
-    print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
-    print("Bienvenido al juego del ahorcado")
-    print("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°")
-
-    print("Fin del juego")
--- juegos.py
+++ juegos.py
@@ -5,11 +5,13 @@ print('================================')
 print('Elija su Juego')
 print('================================')
 
-print("(1) Horca (2) Adivinanza")
+print("(1) Ahorcado (2) Adivinanza")
 
 juego = int(input("¿Cuál juego? "))
 
 if (juego == 1):
     print("Jugando ahorcado")
+    ahorcado.jugar_ahorcado()
 elif (juego == 2):
-    print("Jugando adivinanza")
+    print("Jugando adivinanza")
+    adivinanza.jugar_adivinanza()
